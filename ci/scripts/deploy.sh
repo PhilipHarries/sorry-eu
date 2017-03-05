820ee12ea7f04efa41ba88c5052fd404c41f75dd
Philip Harries <philip.harries.unix.consulting@gmail.com>
2017-03-05 15:42:26 +0000

    fixed deploy script

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -15,6 +15,6 @@ echo user: "${SSH_USER}"
 echo target: "${TARGET_SERVER}"
 echo path: "${DEPLOY_PATH}"
 
-scp -i "${HOME}/.ssh/id_rsa" ./sorryeu-repo/site/* "${SSH_USER}@${TARGET_SERVER}:${DEPLOY_PATH}"
+scp -r -i "${HOME}/.ssh/id_rsa" ./sorryeu-repo/site/* "${SSH_USER}@${TARGET_SERVER}:${DEPLOY_PATH}"
 
 exit 0
